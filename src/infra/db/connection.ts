51f44c2a97da5a62e6b2c0887e4ab313edf557df
Caio Nascimento <caionsantos2000@gmail.com>
2023-09-27 15:36:11 -0300

    feat: ssl connection option

--- src/infra/db/connection.ts
+++ src/infra/db/connection.ts
@@ -1,6 +1,9 @@
 import { Pool } from 'pg';
 import 'dotenv/config';
-const pool = new Pool();
+const ssl = process.env.PGSSL;
+const pool = new Pool({
+  ssl: Boolean(ssl),
+});
 
 pool.on('error', err => {
   console.error('Unexpected error on idle client', err);
