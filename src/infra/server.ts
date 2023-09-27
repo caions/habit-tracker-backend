bfcc44970ed4d38b9b744fe11a0d57d632dd29cf
Caio Nascimento <caionsantos2000@gmail.com>
2023-09-27 17:54:04 -0300

    feat: route log api version

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import express, { Response } from 'express';
+import { version } from '../../package.json';
 import 'dotenv/config';
 import 'express-async-errors';
 import { routes } from './web/routes';
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
-
+app.get('/', (_, res: Response) => res.json('api version: ' + version));
 app.listen(PORT, () => {
   executeMigrations(), console.log(`server runing on localhost:${PORT}`);
 });
